bdaf9fb4ba00ff20a9d5d3c9bb63cad38256f6f6
Sangram Badi <sangram.bachu@gmail.com>
2025-11-18 23:40:12 +0530

    missed content

--- project2/todos.py
+++ project2/todos.py
@@ -1,4 +1,4 @@
-from fastapi import Body, FastAPI, HTTPException
+from fastapi import Body, FastAPI, HTTPException, Query, Path, status
 from TodoModel import Todo
 from TodoRequest import TodoRequest
 from TodoResponse import TodoResponse
@@ -16,12 +16,12 @@ TODOS = [
     Todo(4, "Task 4", "My Task 4", False, 1)
 ]
 
-@app.get("/todos/all", response_model=list[TodoResponse], status_code=200)
+@app.get("/todos/all", response_model=list[TodoResponse], status_code=status.HTTP_200_OK)
 async def get_all():
     return TODOS
 
-@app.get("/todo/get", response_model=list[TodoResponse], status_code=200)
-async def get_by_priority(priority: int):
+@app.get("/todo/get", response_model=list[TodoResponse], status_code=status.HTTP_200_OK)
+async def get_by_priority(priority: int = Query(ge=1, le=5)):
     result = []
 
     for todo in TODOS:
@@ -30,13 +30,13 @@ async def get_by_priority(priority: int):
 
     return result
 
-@app.post("/todos/create", response_model=TodoResponse, status_code=201)
+@app.post("/todos/create", response_model=TodoResponse, status_code=status.HTTP_201_CREATED)
 async def create_todo(todo: TodoRequest):
     t = Todo(**todo.dict())
     TODOS.append(get_todo_id(t))
     return t
 
-@app.put("/todos/update", response_model=TodoResponse, status_code=201)
+@app.put("/todos/update", response_model=TodoResponse, status_code=status.HTTP_201_CREATED)
 async def update_todo(todo: TodoRequest):
     t = Todo(**todo.dict())
     not_found = True
@@ -46,19 +46,19 @@ async def update_todo(todo: TodoRequest):
             return t
 
     if not_found == True:
-        raise HTTPException(status_code=400, detail="Id not found") 
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Id not found") 
 
         
 
-@app.delete("/todos/remove/{id}", status_code=204)
-async def delete_todo(id: int):
+@app.delete("/todos/remove/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_todo(id: int = Path(ge=1)):
     not_found = True
     for index in range(len(TODOS)):
         if TODOS[index].id == id:
             TODOS.pop(index)
             return
     if not_found == True:
-        raise HTTPException(status_code=400, detail="Id not found")  
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Id not found")  
 
         
 
